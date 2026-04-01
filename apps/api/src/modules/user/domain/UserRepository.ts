b113025d2e6fd01fc8668d80a4ff1cc5fea070e7
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-04-02 04:36:28 +1030

    [user-registration] Register user in 'User' table of the database.

--- apps/api/src/modules/user/domain/UserRepository.ts
+++ apps/api/src/modules/user/domain/UserRepository.ts
@@ -2,4 +2,5 @@ import type { User } from "./User.js";
 
 export default interface UserRepository {
   findByEmail: (email: string) => Promise<User | null>;
+  register: (userData: User) => Promise<User | null>;
 }
--- apps/api/src/modules/user/infrastructure/UserRepositoryPrisma.ts
+++ apps/api/src/modules/user/infrastructure/UserRepositoryPrisma.ts
@@ -23,4 +23,23 @@ export const UserRepositoryPrisma: UserRepository = {
       role: record.role,
     });
   },
+  async register(userData: User) {
+    const record = await prisma.user.create({
+      data: {
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        password: userData.password,
+      },
+    });
+
+    return User.create({
+      id: record.id,
+      firstName: record.firstName,
+      lastName: record.lastName,
+      email: record.email,
+      password: record.password,
+      role: record.role,
+    });
+  },
 };
