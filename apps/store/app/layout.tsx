3f56bf5fb4598f6562bb4746e1c027ae11d5879a
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-04-11 18:15:41 +0930

    [default-font] Make google font ArchitectsDaughter as the default font for the whole application.

--- apps/store/app/layout.tsx
+++ apps/store/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Architects_Daughter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/layout";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const architectsDaughter = Architects_Daughter({
+  weight: "400",
+  variable: "--font-architects-daughter",
   subsets: ["latin"],
 });
 
@@ -24,10 +20,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-zinc-700 dark:text-white`}
-      >
+    <html
+      lang="en"
+      className={`${architectsDaughter.variable} antialiased`}
+    >
+      <body className={`dark:bg-zinc-700 dark:text-white`}>
         <Header />
         {children}
       </body>
