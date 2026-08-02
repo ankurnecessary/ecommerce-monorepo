bd09432f87eed4662202272d720cd170d04d51bf
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2026-08-03 04:36:53 +0930

    [user-avatar-dropdown] Show dropdown items will appear differently on logged in basis.

--- apps/store/components/layout/Header/UserAvatarDropdown.tsx
+++ apps/store/components/layout/Header/UserAvatarDropdown.tsx
@@ -3,14 +3,19 @@ import {
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
 } from "@repo/ui/components/dropdown-menu";
 import React from "react";
 import UserAvatar from "./UserAvatar";
 import {
   LogInIcon,
+  LogOutIcon,
+  SettingsIcon,
+  UserIcon,
   UserPlus,
 } from "lucide-react";
 import Link from "next/link";
+import { Show } from "@clerk/nextjs";
 
 const UserAvatarDropdown = () => {
   return (
@@ -26,38 +31,44 @@ const UserAvatarDropdown = () => {
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end">
-        <DropdownMenuItem asChild>
-          <Link href={"/sign-up"} className="inline-flex w-full cursor-pointer">
-            <UserPlus size={16} className="ml-1 mr-2" />
-            Sign up
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href={"#"} className="inline-flex w-full cursor-pointer">
-            <LogInIcon size={16} className="ml-1 mr-2" />
-            Sign in
-          </Link>
-        </DropdownMenuItem>
-
-        {/* <DropdownMenuItem asChild>
-          <Link href={"#"} className="inline-flex w-full cursor-pointer">
-            <UserIcon size={16} className="ml-1 mr-2" />
-            Profile
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href={"#"} className="inline-flex w-full cursor-pointer">
-            <SettingsIcon size={16} className="ml-1 mr-2" />
-            Settings
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <Link href={"#"} className="inline-flex w-full cursor-pointer">
-            <LogOutIcon size={16} className="ml-1 mr-2" />
-            Sign out
-          </Link>
-        </DropdownMenuItem> */}
+        <Show when="signed-out">
+          <DropdownMenuItem asChild>
+            <Link
+              href={"/sign-up"}
+              className="inline-flex w-full cursor-pointer"
+            >
+              <UserPlus size={16} className="ml-1 mr-2" />
+              Sign up
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href={"#"} className="inline-flex w-full cursor-pointer">
+              <LogInIcon size={16} className="ml-1 mr-2" />
+              Sign in
+            </Link>
+          </DropdownMenuItem>
+        </Show>
+        <Show when="signed-in">
+          <DropdownMenuItem asChild>
+            <Link href={"#"} className="inline-flex w-full cursor-pointer">
+              <UserIcon size={16} className="ml-1 mr-2" />
+              Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href={"#"} className="inline-flex w-full cursor-pointer">
+              <SettingsIcon size={16} className="ml-1 mr-2" />
+              Settings
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <Link href={"#"} className="inline-flex w-full cursor-pointer">
+              <LogOutIcon size={16} className="ml-1 mr-2" />
+              Sign out
+            </Link>
+          </DropdownMenuItem>
+        </Show>
       </DropdownMenuContent>
     </DropdownMenu>
   );
